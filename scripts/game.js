f1279e0960c844214bc0a18f80f213ebd6b6c524
Andre Sanches <andrecsanches2@gmail.com>
2022-04-30 17:32:33 -0300

    Added attack(), respawnEnemy() and checkWinner() functions,  changed yellow ninja to blue(water) ninja, added player spawn and playerArray and some other stuff

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,16 @@
+
+class Ninja {
+    constructor(name, sprite, health, strength, defence, type) {
+        this.name = name;
+        this.sprite = sprite;
+        this.health = health;
+        this.fullHealth = health
+        this.strength = strength;
+        this.defence = defence
+        this.type = type
+    }
+}
+
 //----------PLAYER-BOX-DOM--------------------------------------------------------------------------------------------------
 
 let playerName = document.getElementById('playerName')
@@ -5,6 +18,7 @@ let playerHealth = document.getElementById('playerCurrentHealth')
 let playerMaxhealth = document.getElementById('playerMaxHealth')
 let playerSTR = document.getElementById('playerStrength')
 let playerDEF = document.getElementById('playerDefense')
+let battleCommentary = document.getElementById('battleComment')
 
 
 //--------------------------------------------------------------------------------------------------------------------------
@@ -16,6 +30,7 @@ let enemyHealth = document.getElementById('enemyCurrentHealth')
 let enemyMaxhealth = document.getElementById('enemyMaxHealth')
 let enemySTR = document.getElementById('enemyStrength')
 let enemyDEF = document.getElementById('enemyDefense')
+let enemySprite = document.getElementById('enemyImg')
 
 //--------------------------------------------------------------------------------------------------------------------------
 
@@ -25,40 +40,56 @@ let btnFire = document.querySelector('#btnFire')
 let btnLeaf = document.querySelector('#btnLeaf')
 let btnRock = document.querySelector('#btnRock')
 
-
 //--------------------------------------------------------------------------------------------------------------------------
 
 //-----------MONSTERS-ARRAY-------------------------------------------------------------------------------------------------
 
 const monstersArray = [
-    ['Green Ninja', '/charactersMedia/greenTeam/GreenNinja/Faceset.png', 22, 6, 2],
-    ['Yellow Ninja', '/charactersMedia/yellowTeam/Yellowknight/Faceset.png', 30, 12, 2],
-    ['Red Ninja', '/charactersMedia/redTeam/RedNinja2/Faceset.png', 48, 4, 8]
+    ['Leaf Ninja', '/charactersMedia/GreenNinja/Faceset.png', 22, 6, 2, 'leaf'],
+    ['Water Ninja', '/charactersMedia/BlueNinja/Faceset.png', 30, 6, 6, 'earth'],
+    ['Fire Ninja', '/charactersMedia/RedNinja/Faceset.png', 30, 8, 2, 'fire']
 ]
-
-let typeMatch = {
-    'Green Ninja': [['leaf'], ['fire'], ['earth']],
-    'Yellow Ninja': [['earth'], ['fire'], ['leaf']],
-    'Red Ninja': [['fire'], ['earth'], ['leaf']]
-}
 
 //--------------------------------------------------------------------------------------------------------------------------
 
-class Ninja {
-    constructor(name, sprite, health, strength, defence) {
-        this.name = name;
-        this.sprite = sprite;
-        this.health = health;
-        this.fullHealth = health
-        this.strength = strength;
-        this.defence = defence
-    }
+//---------PLAYER-SPAWN-----------------------------------------------------------------------------------------------------
+
+const playerArray = [
+    ['Black Ninja', '/charactersMedia/Player/DarkNinja/Faceset.png', 20, 10, 4]
+]
+
+let player = playerArray[0]
+let ninjaPlayer = new Ninja(player[0], player[1], player[2], player[3], player[4])
+
+
+playerName.innerHTML = ninjaPlayer.name
+playerHealth.innerHTML = ninjaPlayer.health
+playerMaxhealth.innerHTML = ninjaPlayer.health
+playerSTR.innerHTML = ninjaPlayer.strength
+playerDEF.innerHTML = ninjaPlayer.defence
+
+playerName = ninjaPlayer.name
+playerMaxhealth = ninjaPlayer.health
+playerSTR = ninjaPlayer.strength
+playerDEF = ninjaPlayer.defence
+
+//--------------------------------------------------------------------------------------------------------------------------
+//---------ENEMY-SPAWN/RESPAWN----------------------------------------------------------------------------------------------
+
+function respawnEnemy() {
+    spawnedNinjaEnemy = spawn()
+    playerAttackDMG = playerSTR - spawnedNinjaEnemy.defence
+    enemyAttackDamage = spawnedNinjaEnemy.strength - ninjaPlayer.defence
+
+    return spawnedNinjaEnemy
 }
+
 
 function spawn() {
     let enemy = monstersArray[Math.floor(Math.random() * monstersArray.length)]
-    let ninjaEnemy = new Ninja(enemy[0], enemy[1], enemy[2], enemy[3], enemy[4],)
+    let ninjaEnemy = new Ninja(enemy[0], enemy[1], enemy[2], enemy[3], enemy[4], enemy[5])
 
+    enemySprite.setAttribute('src', ninjaEnemy.sprite)
     enemyName.innerHTML = ninjaEnemy.name
     enemyHealth.innerHTML = ninjaEnemy.health
     enemyMaxhealth.innerHTML = ninjaEnemy.health
@@ -67,15 +98,79 @@ function spawn() {
 
     return ninjaEnemy
 }
+let spawnedNinjaEnemy = spawn()
 
-function attack(str, def, attacker, receiver, health, owner) {
+//--------------------------------------------------------------------------------------------------------------------------
+
+//-----ENEMY-AND-PLAYER-ATTACK-DAMAGE-ATTRIBUTION---------------------------------------------------------------------------
+
+
+let enemyAttackDamage = spawnedNinjaEnemy.strength - ninjaPlayer.defence
+let playerAttackDMG = playerSTR - spawnedNinjaEnemy.defence
+
+//--------------------------------------------------------------------------------------------------------------------------
+
+function attack(str, def, attacker, receiver, health) {
+    // O ataque do personagem pega a força dele e retira o HP do inimigo
+    // Se a vida do inimigo for maior que 0, ele ataca 2 segundos depois
+    // As informaçoes dos ataques tem que ser exibidas na tela 
+    // No final do ataque, a função chama a função checkWinner() 
+    // disableButtons()
+    // setTimeout(() => {enableButtons}, 300);
+    spawnedNinjaEnemy.health = spawnedNinjaEnemy.health - playerAttackDMG
+    enemyHealth.innerHTML = spawnedNinjaEnemy.health
+
+    battleCommentary.innerHTML = `The player has attacked ${spawnedNinjaEnemy.name}, dealing ${playerAttackDMG} damage`
+
+    if (spawnedNinjaEnemy.health > 0) {
+        setTimeout(() => {
+            ninjaPlayer.health = ninjaPlayer.health - enemyAttackDamage
+            playerHealth.innerHTML = ninjaPlayer.health
+
+            battleCommentary.innerHTML = `${spawnedNinjaEnemy.name} has attacked the player, dealing ${enemyAttackDamage} damage`
+        }, 2000)
+    }      
+    
+    checkWinner()
 
 }
 
-function checkWinner(health) {
+function checkWinner() {
+    setTimeout(() => {
+        if (ninjaPlayer.health <= 0) {
+            battleCommentary.innerHTML = "The player's HP reached 0"
+            setTimeout(() => {
+                battleCommentary.innerHTML = 'GAME OVER'
+            }, 2000)
+        }
+    }, 3000)
+     if (spawnedNinjaEnemy.health <= 0) {
+        battleCommentary.innerHTML = `${spawnedNinjaEnemy.name} has fainted`
+        setTimeout(() => {
+            battleCommentary.innerHTML = `Another enemy has appeared`
+            respawnEnemy()
 
+        }, 2000);
+        setTimeout(() => {
+            battleCommentary.innerHTML = ''
+        }, 4000)
+    }
 }
 
-btnFire.addEventListener('click', spawn)
+btnFire.addEventListener('click', respawnEnemy)
 btnLeaf.addEventListener('click', attack)
-btnEarth.addEventListener('click', attack)
+btnWater.addEventListener('click', attack)
+
+
+function enableButtons() {
+
+    btnFire.removeAttribute('disabled')
+    btnLeaf.removeAttribute('disabled')
+    btnWater.removeAttribute('disabled')
+}
+
+function disableButtons() {
+    btnFire.setAttribute('disabled', true)
+    btnLeaf.setAttribute('disabled', true)
+    btnWater.setAttribute('disabled', true)
+}
